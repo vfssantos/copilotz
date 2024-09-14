32f10b86f66a571be4ad37c5d33e5cf52f02f2f1
Vinicius Santos <vfssantos@outlook.com>
2024-09-14 11:56:47 -0300

    adding logs schema to git

---
+++ src/schemas/logs/main.js
@@ -0,0 +1,13 @@
+export default {
+    "_id": "number",
+    "name": "string",
+    "url": "string",
+    "requestId": "string",
+    "executionId": "string",
+    "tags": "any",
+    "input": "any",
+    "output": "any",
+    "duration": "number",
+    "status": "string", // started, completed, failed
+    "hidden": "boolean", 
+}
