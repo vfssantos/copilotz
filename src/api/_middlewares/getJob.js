41360cba7599517c733c9cfaafb03694c1707d49
Vinicius Santos <vfssantos@outlook.com>
2024-09-14 12:13:00 -0300

    commenting request validation

--- src/api/_middlewares/getJob.js
+++ src/api/_middlewares/getJob.js
@@ -8,7 +8,6 @@ const getJob = async (req) => {
 
     const job = await models.jobs.findOne({ _id: copilotz.job }, { populate: ['actions'] });
 
-
     const workflow = await models.workflows.findOne({ _id: job.defaultWorkflow }, { populate: ['steps'] });
 
     job.defaultWorkflow = workflow;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,61 +1,61 @@
 const middlewares: any = async (req: any) => {
 
-    const { env } = middlewares;
+    // const { env } = middlewares;
 
-    env.ENV === 'production' && await validateRequest(req, env.PUBLIC_KEY);
+    // env.ENV === 'production' && await validateRequest(req, env.PUBLIC_KEY);
 
     return req;
 }
 
 export default middlewares;
 
-const validateRequest = async (req: any, publicKeyStr: string) => {
+// const validateRequest = async (req: any, publicKeyStr: string) => {
 
-    const signature = decodeBase64(req.headers.get("x-signature") || "");
+//     const signature = decodeBase64(req.headers.get("x-signature") || "");
 
-    const payload = req.params;
+//     const payload = req.params;
 
-    // Convert the PEM encoded public key to a CryptoKey object
-    const publicKey = await crypto.subtle.importKey(
-        "spki",
-        decodePem(publicKeyStr),
-        {
-            name: "ECDSA",
-            namedCurve: "P-256", // Ensure this matches the curve used for signing
-        },
-        true,
-        ["verify"]
-    );
+//     // Convert the PEM encoded public key to a CryptoKey object
+//     const publicKey = await crypto.subtle.importKey(
+//         "spki",
+//         decodePem(publicKeyStr),
+//         {
+//             name: "ECDSA",
+//             namedCurve: "P-256", // Ensure this matches the curve used for signing
+//         },
+//         true,
+//         ["verify"]
+//     );
 
-    // Verify the signature
-    const isValid = await crypto.subtle.verify(
-        {
-            name: "ECDSA",
-            hash: { name: "SHA-256" }, // Ensure this matches the hash function used for signing
-        },
-        publicKey,
-        signature,
-        new TextEncoder().encode(payload)
-    );
+//     // Verify the signature
+//     const isValid = await crypto.subtle.verify(
+//         {
+//             name: "ECDSA",
+//             hash: { name: "SHA-256" }, // Ensure this matches the hash function used for signing
+//         },
+//         publicKey,
+//         signature,
+//         new TextEncoder().encode(payload)
+//     );
 
-    if (!isValid) {
-        throw { status: 401, message: "Invalid signature" };
-    }
+//     if (!isValid) {
+//         throw { status: 401, message: "Invalid signature" };
+//     }
 
-    return
-}
+//     return
+// }
 
 
-// Helper function to decode a PEM formatted string to a Uint8Array
-function decodePem(pem: string): Uint8Array {
-    const base64 = pem
-        .replace(/-----BEGIN PUBLIC KEY-----/, "")
-        .replace(/-----END PUBLIC KEY-----/, "")
-        .replace(/\s+/g, "");
-    return Uint8Array.from(atob(base64), c => c.charCodeAt(0));
-}
+// // Helper function to decode a PEM formatted string to a Uint8Array
+// function decodePem(pem: string): Uint8Array {
+//     const base64 = pem
+//         .replace(/-----BEGIN PUBLIC KEY-----/, "")
+//         .replace(/-----END PUBLIC KEY-----/, "")
+//         .replace(/\s+/g, "");
+//     return Uint8Array.from(atob(base64), c => c.charCodeAt(0));
+// }
 
-// Helper function to decode a Base64 encoded string to a Uint8Array
-function decodeBase64(data: string): Uint8Array {
-    return Uint8Array.from(atob(data), c => c.charCodeAt(0));
-}
+// // Helper function to decode a Base64 encoded string to a Uint8Array
+// function decodeBase64(data: string): Uint8Array {
+//     return Uint8Array.from(atob(data), c => c.charCodeAt(0));
+// }
