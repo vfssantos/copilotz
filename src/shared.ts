008a98c646973da7626106c0c6146dbe8c709b11
Vinicius Santos <vfssantos@outlook.com>
2024-09-25 22:07:39 -0300

    remove unnecessary comments

--- src/shared.ts
+++ src/shared.ts
@@ -4,27 +4,7 @@ import * as security from 'axion-modules/connectors/security.js';
 
 export default ({ env, ...modules }: Record<string, any>) => {
 
-    // Create both local models and remote models
-    // Local models will be used for reading data
-    // Remote models will be used for writing data
-
-    // const localModels = crud({
-    //     schemas,
-    //     config: {
-    //         dbPath: `file:${Deno.cwd()}/${env.LOCAL_DB_PATH}`,
-    //         dbOptions: {
-    //             authToken: env.DB_TOKEN,
-    //             syncUrl: env.DB_PATH,
-    //             syncPeriod: 1,
-    //         },
-    //         debug: false,
-    //         serializer: 'stringifyArrays',
-    //         addTimestamps: true
-    //     }
-    // });
-
-    console.log('connecting to db in client');
-    const remoteModels = crud({
+    const models = crud({
         schemas,
         config: {
             dbPath: env.DB_PATH,
@@ -36,22 +16,6 @@ export default ({ env, ...modules }: Record<string, any>) => {
             addTimestamps: true
         }
     });
-
-    // const models: Record<string, any> = {};
-    // Object.keys(schemas).forEach((key) => {
-    //     models[key] = {
-    //         create: remoteModels[key].create,
-    //         createMany: remoteModels[key].createMany,
-    //         find: localModels[key].find,
-    //         findOne: localModels[key].findOne,
-    //         update: remoteModels[key].update,
-    //         updateMany: remoteModels[key].updateMany,
-    //         delete: remoteModels[key].delete,
-    //         deleteMany: remoteModels[key].deleteMany,
-    //     }
-    // });
-
-    const models=remoteModels;
 
     const utils = { security }
 
