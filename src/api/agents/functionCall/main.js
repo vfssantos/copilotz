6d63d8f9601fca508d073b856588e038946defff
Vinicius Santos <vfssantos@outlook.com>
2024-11-08 16:02:39 -0300

    fixing functionCall agent with no action modules

--- src/api/agents/functionCall/main.js
+++ src/api/agents/functionCall/main.js
@@ -31,6 +31,7 @@ const functionCall = async (
   console.log(`[functionCall] Starting iteration ${iterations}`);
 
   let actions = {};
+  actionModules = actionModules || {};
 
   // 1. Extract Modules, Resources, Utils, and Dependencies
   const { modules, resources, utils, env } = functionCall;
@@ -132,7 +133,7 @@ const functionCall = async (
   // 6. Get Thread Logs
   console.log(`[functionCall] Fetching thread history`);
   if (!threadLogs || !threadLogs?.length) {
-    const lastLog = await getThreadHistory(thread.extId, { functionName: 'functionCall', maxRetries: 10 })
+    const lastLog = await`` getThreadHistory(thread.extId, { functionName: 'functionCall', maxRetries: 10 })
     if (lastLog) {
       const { prompt, ...agentResponse } = lastLog;
       threadLogs = prompt || [];
